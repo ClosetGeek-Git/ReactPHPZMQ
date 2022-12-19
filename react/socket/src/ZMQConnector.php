a4b49f76000ca0d12d6700cfd110ed194e64daf3
JSON <jasonrlester@yahoo.com>
2022-12-18 16:03:15 -0800

    Update ZMQConnector.php

--- react/socket/src/ZMQConnector.php
+++ react/socket/src/ZMQConnector.php
@@ -17,23 +17,17 @@ final class ZMQConnector implements ConnectorInterface
     private $zmq_socket = null;
     private $bound = false;
 
-    public function __construct(LoopInterface $loop = null, $config = null)
-    {
+    public function __construct(LoopInterface $loop = null, $config = null) {
         $this->loop = $loop ?: Loop::get();
         
-        if($config == null || !is_array($config))
-        {
+        if($config == null || !is_array($config)) {
             throw new InvalidArgumentException('Must provide a valid configuration in second argument');
         }
-
-        if(!isset($config['type']))
-        {
+        if(!isset($config['type'])) {
             throw new InvalidArgumentException('Cannot create ZMQConnector without a zmq socket type');
         
-        }else
-        {
-            switch($config['type'])
-            {
+        }else {
+            switch($config['type']) {
                 case \ZMQ::SOCKET_PAIR:
                 case \ZMQ::SOCKET_PUB:
                 case \ZMQ::SOCKET_SUB:
@@ -50,71 +44,52 @@ final class ZMQConnector implements ConnectorInterface
                 case \ZMQ::SOCKET_STREAM:
                 case \ZMQ::SOCKET_UPSTREAM:
                 case \ZMQ::SOCKET_DOWNSTREAM:
-                {
                     $this->zmq_socket_type = $config['type'];
-                }
                 break;
                 default:
-                {
                     throw new InvalidArgumentException('Not a valid ZMQ Socket type');    
-                }
             }
         }
 
-        if (isset($config['context']))
-        {
-            if ($config['context'] instanceof \ZMQContext)
-            {
+        if(isset($config['context'])) {
+            if($config['context'] instanceof \ZMQContext) {
                 $this->context = $config['context'];
             
-            } else
-            {
+            }else {
                 throw new InvalidArgumentException('\'context\' is set in configuration argument but is not a valid ZMQContext');
             }
         
-        } else
-        {
+        }else {
             $this->context = \ZMQContext::acquire();
         }
 
         $this->zmq_socket = new \ZMQSocket($this->context, $this->zmq_socket_type);
-
         $this->config = $config;
-        
     }
 
     public function bind($path)
     {
-        try
-        {
+        try {
             $resource = $this->zmq_socket->bind($path);
-            
-        } catch(Exception $e)
-        {
+        }catch(Exception $e) {
             return Promise\reject(new \RuntimeException("Unable to bind ZMQ socket: ". $e->getMessage() ));            
         }        
 
         $connection = new ZMQConnection($resource, $this->loop);
         $connection->resource_id = spl_object_hash($resource);;
-
         return Promise\resolve($connection);
     }
 
-    public function connect($path)
-    {   
+    public function connect($path) {   
         $resource = NULL;
-        try
-        {
+        try {
             $resource = $this->zmq_socket->connect($path);
-        
-        } catch(Exception $e)
-        {
+        }catch(Exception $e) {
             return Promise\reject(new \RuntimeException("Unable to connect ZMQ socket: ". $e->getMessage() ));            
         }        
 
         $connection = new ZMQConnection($resource, $this->loop);
         $connection->resource_id = spl_object_hash($resource);;
-
         return Promise\resolve($connection);
     }
 }
